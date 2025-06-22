a4a06f258b0341120dc6d867ac546382b082ae85
codewithomarm <codewithomarm@gmail.com>
2025-06-22 12:59:41 -0500

    Creacion de la clase modelo Roles

--- src/main/java/com/isaiiapp/backend/auth/v1/roles/model/Roles.java
+++ src/main/java/com/isaiiapp/backend/auth/v1/roles/model/Roles.java
@@ -2,6 +2,7 @@ package com.isaiiapp.backend.auth.v1.roles.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(schema = "auth", name = "roles",
         uniqueConstraints = {
-
+            @UniqueConstraint(columnNames = "name", name = "roles_name_UNIQUE")
         })
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,6 +22,7 @@ public class Roles {
     private Long id;
 
     @NotNull(message = "Name should not be null")
+    @Size(min = 4, max = 50, message = "Name should be between 4 and 50 characters")
     @Column(unique = true, nullable = false, length = 50)
     private String name;
 
