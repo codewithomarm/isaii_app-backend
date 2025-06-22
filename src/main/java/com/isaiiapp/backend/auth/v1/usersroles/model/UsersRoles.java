0c33abb98f8202886233052c19bea8f65f7b1745
codewithomarm <codewithomarm@gmail.com>
2025-06-22 13:01:30 -0500

    Creacion de la clase modelo usersroles

---
+++ src/main/java/com/isaiiapp/backend/auth/v1/usersroles/model/UsersRoles.java
@@ -0,0 +1,29 @@
+package com.isaiiapp.backend.auth.v1.usersroles.model;
+
+import com.isaiiapp.backend.auth.v1.roles.model.Roles;
+import com.isaiiapp.backend.auth.v1.users.model.Users;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(schema = "auth", name = "users_roles")
+public class UsersRoles {
+
+    @EmbeddedId
+    @NotNull(message = "Users Roles id should not be null")
+    private UsersRolesId id;
+
+    @ManyToOne
+    @MapsId("userId")
+    @NotNull(message = "User should not be null")
+    @JoinColumn(name = "users_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_users_roles_users"))
+    private Users user;
+
+    @ManyToOne
+    @MapsId("roleId")
+    @NotNull(message = "Role should not be null")
+    @JoinColumn(name = "roles_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_users_roles_roles"))
+    private Roles role;
+}
---
+++ src/main/java/com/isaiiapp/backend/auth/v1/usersroles/model/UsersRolesId.java
@@ -0,0 +1,37 @@
+package com.isaiiapp.backend.auth.v1.usersroles.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UsersRolesId implements Serializable {
+
+    @NotNull(message = "User id should not be null")
+    private Long userId;
+
+    @NotNull(message = "Role id should not be null")
+    private Long roleId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersRolesId that = (UsersRolesId) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(roleId, that.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, roleId);
+    }
+}
