e315e79b9a47690c9d966b0af9e21dc65fd1d4d1
codewithomarm <codewithomarm@gmail.com>
2025-07-17 23:29:37 -0500

    Corrigiendo SecurityConfig

--- src/main/java/com/isaiiapp/backend/security/v1/config/SecurityConfig.java
+++ src/main/java/com/isaiiapp/backend/security/v1/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/v1/auth/login",
+                                "/v1/auth/login",
                                 "/api/v1/auth/refresh",
                                 "/actuator/health",
                                 "/actuator/info"
