72ff3c9b0ef8e1423ffceddca4c62edb8fc29093
codewithomarm <codewithomarm@gmail.com>
2025-07-30 00:29:12 -0500

    Modificando Securityconfig para CORS

--- src/main/java/com/isaiiapp/backend/security/v1/config/SecurityConfig.java
+++ src/main/java/com/isaiiapp/backend/security/v1/config/SecurityConfig.java
@@ -227,7 +227,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5500")); // cambia esto según tu dominio real
+        config.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500")); // cambia esto según tu dominio real
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
